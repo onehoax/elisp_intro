ff4763c83e58d01299f577ff881439caefd6d6cc
Andres Osorio <andres.osoriogr@gmail.com>
2026-05-17 17:48:45 -0500

    yanking

--- functions.el
+++ functions.el
@@ -123,3 +123,32 @@ colors
 
 (third-element-killring)
 
+kill-ring-max
+
+;Set ‘flowers’ to ‘violet’ and ‘buttercup’.  Cons two more flowers on to
+;this list and set this new list to ‘more-flowers’.  Set the CAR of
+;‘flowers’ to a fish.  What does the ‘more-flowers’ list now contain?
+
+(setq flowers (list 'violet 'buttercup))
+
+(setq more-flowers (cons 'rose (cons 'orchid flowers)))
+
+flowers
+more-flowers
+
+(setcar flowers 'trout)
+
+flowers
+more-flowers
+
+;Using ‘nthcdr’ and ‘car’, construct a series of expressions to
+;return the first, second, third, and fourth elements of a list.
+
+(setq colors (list 'red 'blue 'yellow 'green 'gray))
+
+colors
+
+(car colors)
+(car (nthcdr 1 colors))
+(car (nthcdr 2 colors))
+
